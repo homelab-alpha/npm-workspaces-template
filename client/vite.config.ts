a8c349bdb55ab2991392ddf9b484beb0736fa6d0
GJS <homelab.api@gmail.com>
2025-08-05 12:53:05 +0200

    feat(vite): add server config, crucial for the Docker setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,4 +13,10 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    // This allows the Vite dev server to be accessed from outside the container.
+    // It's crucial for the Docker setup.
+    host: "0.0.0.0",
+    port: 5173,
+  },
 });
